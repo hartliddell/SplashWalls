465dd3a4cc7e3cbe4419b72625816bc76d81a772
Hart Liddell <hart@leapingfish.io>
2016-08-23 15:33:02 -0400

    double tap detected

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,19 @@
  * https://www.smashingmagazine.com/2016/04/the-beauty-of-react-native-building-your-first-ios-app-with-javascript-part-1/
  * @flow
  */
-
-import { AppRegistry, StyleSheet, Text, View, ActivityIndicatorIOS } from 'react-native';
+'use strict';
+import { ActivityIndicator, AppRegistry, Dimensions, PanResponder, StyleSheet, Text, View } from 'react-native';
+import NetworkImage from 'react-native-image-progress';
+import * as Progress from 'react-native-progress';
 import RandManager from './RandManager';
+import Utils from './Utils';
 import React, { Component } from 'react';
 import Swiper from 'react-native-swiper';
 
+const { width, height } = Dimensions.get('window');
 const NUM_WALLPAPERS = 5;
+const DOUBLE_TAP_DELAY = 300; // milliseconds
+const DOUBLE_TAP_RADIUS = 20;
 
 class SplashWalls extends Component {
 
@@ -21,10 +27,55 @@ class SplashWalls extends Component {
           wallsJSON: [],
           isLoading: true
         };
+
+        this.imagePanResponder = {};
+        this.prevTouchInfo = {
+            prevTouchX: 0,
+            prevTouchY: 0,
+            prevTouchTimeStamp: 0
+        };
     }
 
     componentDidMount() {
         this.fetchJallsJSON();
+    }
+
+    componentWillMount() {
+        this.imagePanResponder = PanResponder.create({
+            onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+            onPanResponderGrant: this.handlePanResponderGrant.bind(this),
+            onPanResponderRelease: this.handlePanResponderEnd,
+            onPanResponderTerminate: this.handlePanResponderEnd
+        });
+    }
+
+    isDoubleTap(currentTouchTimeStamp, {x0, y0}) {
+        var {prevTouchX, prevTouchY, prevTouchTimeStamp} = this.prevTouchInfo;
+        var dt = currentTouchTimeStamp - prevTouchTimeStamp;
+        return (dt < DOUBLE_TAP_DELAY && Utils.distance(prevTouchX, prevTouchY, x0, y0) < DOUBLE_TAP_RADIUS);
+    }
+
+    handleStartShouldSetPanResponder(e, gestureState) {
+        return true;
+    }
+
+    handlePanResponderGrant(e, gestureState) {
+
+        var currentTouchTimeStamp = Date.now();
+
+        console.log(this);
+        if( this.isDoubleTap(currentTouchTimeStamp, gestureState) )
+            console.log('Double tap detected');
+
+        this.prevTouchInfo = {
+            prevTouchX: gestureState.x0,
+            prevTouchY: gestureState.y0,
+            prevTouchTimeStamp: currentTouchTimeStamp
+        };
+    }
+
+    handlePanResponderEnd(e, gestureState) {
+        console.log('Finger pulled up from the image');
     }
 
     fetchJallsJSON() {
@@ -48,43 +99,46 @@ class SplashWalls extends Component {
 
     renderLoadingMessage() {
         return (
-            <View style={styles.loadingContainer}>
-                <ActivityIndicatorIOS
+            <View style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: '#F5FCFF',
+            }}>
+                <ActivityIndicator
                   animating={true}
                   color={'#999'}
                   size={'small'}
                   style={{margin: 15}} />
                   <Text style={{color: '#fff'}}>Contacting Unsplash</Text>
-
             </View>
         );
     }
 
     renderResults() {
-        var {wallsJSON, isLoading} = this.state;
-        if( !isLoading ) {
+        var { wallsJSON, isLoading } = this.state;
+        if (!isLoading) {
             return (
-                <Swiper onMomentumScrollEnd={this.onMomentumScrollEnd}>
+                <Swiper onMomentumScrollEnd={this.onMomentumScrollEnd}
+                    dot={<View style={{backgroundColor:'rgba(255,255,255,.4)', width: 8, height: 8,borderRadius: 10, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />}
+                    activeDot={<View style={{backgroundColor: '#fff', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+                    >
                     {wallsJSON.map((wallpaper, index) => {
-                        console.log(wallpaper);
-                        const styles = StyleSheet.create({
-                            slide: {
-                                flex: 1,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                backgroundColor: '#9DD6EB',
-                            },
-                            text: {
-                                color: '#999',
-                                fontSize: 30,
-                                fontWeight: 'bold',
-                            }
-                        });
                         return(
                             <View key={index}>
-                                <Text style={styles.text}>
-                                    {wallpaper.author}
-                                </Text>
+                                <NetworkImage
+                                    source={{uri: `https://unsplash.it/${wallpaper.width}/${wallpaper.height}?image=${wallpaper.id}`}}
+                                    indicator={Progress.Circle}
+                                    style={styles.wallpaperImage}
+                                    indicatorProps={{
+                                        color: 'rgba(255, 255, 255)',
+                                        size: 60,
+                                        thickness: 7
+                                    }}
+                                    {...this.imagePanResponder.panHandlers}>
+                                    <Text style={styles.label}>Photo by</Text>
+                                    <Text style={styles.label_authorName}>{wallpaper.author}</Text>
+                                </NetworkImage>
                             </View>
                         );
                     })}
@@ -102,49 +156,43 @@ class SplashWalls extends Component {
     }
 }
 
-const slideStyles = StyleSheet.create({
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var styles = StyleSheet.create({
+    loadingContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
+    },
+    wallpaperImage: {
+        flex: 1,
+        width: width,
+        height: height,
+        backgroundColor: '#000'
+    },
+    label: {
+        position: 'absolute',
+        color: '#fff',
+        fontSize: 13,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 2,
+        paddingLeft: 5,
+        top: 20,
+        left: 20,
+        width: width/2
+    },
+    label_authorName: {
+        position: 'absolute',
+        color: '#fff',
+        fontSize: 15,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        padding: 2,
+        paddingLeft: 5,
+        top: 41,
+        left: 20,
+        fontWeight: 'bold',
+        width: width/2
+    }
 });
 
 AppRegistry.registerComponent('SplashWalls', () => SplashWalls);
